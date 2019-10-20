ce760d8f7bc1d874618de48aef2884e536f44585
Sultan Alsawaf <sultan@kerneltoast.com>
2019-10-20 11:03:53 -0700

    iommu: msm: Rewrite to improve clarity and performance
    This scope of this driver's lock usage is extremely wide, leading to
    excessively long lock hold times. Additionally, there is lots of
    excessive linked-list traversal and unnecessary dynamic memory
    allocation in a critical path, causing poor performance across the
    board.
    
    Fix all of this by greatly reducing the scope of the locks used and by
    significantly reducing the amount of operations performed when
    msm_dma_map_sg_attrs() is called. The entire driver's code is overhauled
    for better cleanliness and performance.
    
    Note that ION must be modified to pass a known structure via the private
    dma_buf pointer, so that the iommu driver can prevent races when
    operating on the same buffer concurrently. This is the only way to
    eliminate said buffer races without hurting the iommu driver's
    performance.
    
    Some additional members are added to the device struct as well to make
    these various performance improvements possible.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -201,6 +201,8 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 	if (!buffer)
 		return ERR_PTR(-ENOMEM);
 
+	INIT_LIST_HEAD(&buffer->iommu_data.map_list);
+	mutex_init(&buffer->iommu_data.lock);
 	buffer->heap = heap;
 	buffer->flags = flags;
 	kref_init(&buffer->ref);
@@ -300,7 +302,7 @@ static void _ion_buffer_destroy(struct kref *kref)
 	struct ion_heap *heap = buffer->heap;
 	struct ion_device *dev = buffer->dev;
 
-	msm_dma_buf_freed(buffer);
+	msm_dma_buf_freed(&buffer->iommu_data);
 
 	mutex_lock(&dev->buffer_lock);
 	rb_erase(&buffer->node, &dev->buffers);
@@ -1185,7 +1187,7 @@ static struct sg_table *ion_map_dma_buf(struct dma_buf_attachment *attachment,
 					enum dma_data_direction direction)
 {
 	struct dma_buf *dmabuf = attachment->dmabuf;
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 	struct sg_table *table;
 
 	table = ion_dupe_sg_table(buffer->sg_table);
@@ -1323,7 +1325,7 @@ static struct vm_operations_struct ion_vma_ops = {
 
 static int ion_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 	int ret = 0;
 
 	if (!buffer->heap->ops->map_user) {
@@ -1359,14 +1361,14 @@ static int ion_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 
 static void ion_dma_buf_release(struct dma_buf *dmabuf)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 
 	ion_buffer_put(buffer);
 }
 
 static void *ion_dma_buf_kmap(struct dma_buf *dmabuf, unsigned long offset)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 
 	return buffer->vaddr + offset * PAGE_SIZE;
 }
@@ -1380,7 +1382,7 @@ static int ion_dma_buf_begin_cpu_access(struct dma_buf *dmabuf, size_t start,
 					size_t len,
 					enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 	void *vaddr;
 
 	if (!buffer->heap->ops->map_kernel) {
@@ -1399,7 +1401,7 @@ static void ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf, size_t start,
 				       size_t len,
 				       enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 
 	mutex_lock(&buffer->lock);
 	ion_buffer_kmap_put(buffer);
@@ -1441,7 +1443,7 @@ static struct dma_buf *__ion_share_dma_buf(struct ion_client *client,
 	if (lock_client)
 		mutex_unlock(&client->lock);
 
-	dmabuf = dma_buf_export(buffer, &dma_buf_ops, buffer->size, O_RDWR,
+	dmabuf = dma_buf_export(&buffer->iommu_data, &dma_buf_ops, buffer->size, O_RDWR,
 				NULL);
 	if (IS_ERR(dmabuf)) {
 		ion_buffer_put(buffer);
@@ -1499,13 +1501,13 @@ static struct ion_handle *__ion_import_dma_buf(struct ion_client *client,
                 return ERR_CAST(dmabuf);
         /* if this memory came from ion */
 
-        if (dmabuf->ops != &dma_buf_ops) {
-                pr_err("%s: can not import dmabuf from another exporter\n",
-                       __func__);
-                dma_buf_put(dmabuf);
-                return ERR_PTR(-EINVAL);
-        }
-        buffer = dmabuf->priv;
+	if (dmabuf->ops != &dma_buf_ops) {
+		pr_err("%s: can not import dmabuf from another exporter\n",
+		       __func__);
+		dma_buf_put(dmabuf);
+		return ERR_PTR(-EINVAL);
+	}
+	buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 
         if (lock_client)
 		mutex_lock(&client->lock);
@@ -1568,7 +1570,7 @@ static int ion_sync_for_device(struct ion_client *client, int fd)
 		dma_buf_put(dmabuf);
 		return -EINVAL;
 	}
-	buffer = dmabuf->priv;
+	buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
 
 	if (get_secure_vmid(buffer->flags) > 0) {
 		pr_err("%s: cannot sync a secure dmabuf\n", __func__);
